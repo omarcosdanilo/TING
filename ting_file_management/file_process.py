27dee91d9a33137a477393f7b329f25224974204
Marcos Danilo <marcosdanilo94@gmail.com>
2022-12-08 19:17:29 -0300

    feat: implementa a função process no módulo file_process
    a função process é capaz de transformar o conteúdo da lista gerada pela função txt_importer em um dicionário que será armazenado numa fila instanciada da classe Queue do módulo queue. A função não permite armazenar dados do mesmo arquivo mais de uma vez

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,27 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(instance.__len__()):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    news = txt_importer(path_file)
+    nome_do_arquivo = path_file
+    quantidade_de_linhas = len(news)
+
+    processed_data = (
+        {
+            "nome_do_arquivo": nome_do_arquivo,
+            "qtd_linhas": quantidade_de_linhas,
+            "linhas_do_arquivo": news
+        }
+    )
+
+    instance.enqueue(processed_data)
+
+    sys.stdout.write(f"{processed_data}")
 
 
 def remove(instance):
