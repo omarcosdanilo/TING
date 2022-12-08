822c8118b1922a13a7fe79dab0c6e9facc7fe5e6
Marcos Danilo <marcosdanilo94@gmail.com>
2022-12-08 19:55:48 -0300

    feat: implementa a função file_metadata do módulo file_process
    a função recebe uma instância de uma fila e um index, caso o index seja válido as informações do arquivo processado correspondente são impressas via stdout, se o index for inválido a mensagem:  "Posição inválida" é impressa via stderr

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -29,10 +29,16 @@ def remove(instance):
 
     removed_processed_file = instance.dequeue()
 
-    path_file = removed_processed_file['nome_do_arquivo']
+    path_file = removed_processed_file["nome_do_arquivo"]
 
     sys.stdout.write(f"Arquivo {path_file} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position not in range(instance.__len__()):
+        print("Posição inválida", file=sys.stderr)
+        return None
+
+    processed_file_metadata = instance.search(position)
+
+    sys.stdout.write(str(processed_file_metadata))
