8740b5eb498209b620c3722f009be74baed8488b
Marcos Danilo <marcosdanilo94@gmail.com>
2022-12-08 17:47:40 -0300

    feat: implementa uma fila para armazenar arquivos que serão lidos

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,24 @@
+from collections import deque
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.__data = deque()
+
+    def __repr__(self) -> str:
+        return str(self.__data)
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.__data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.__data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.__data.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if (index < 0) | (index > len(self.__data)):
+            raise IndexError
+        else:
+            return self.__data[index]
