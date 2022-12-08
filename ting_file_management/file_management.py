5c12a7fc889c6c01fbf26d28d305dac14de8888a
Marcos Danilo <marcosdanilo94@gmail.com>
2022-12-08 18:28:15 -0300

    feat: cria a função txt_importer
    a função txt_importer é capaz de importar notícias a partir de um arquivo TXT e retorna as notícias numa lista

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    ...
+    if ".txt" not in path_file:
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, "r") as file:
+            content = file.readlines()
+
+        return [text.strip('\n') for text in content if text != '\n']
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
