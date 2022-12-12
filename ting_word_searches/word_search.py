a810f48b95af3160fdd5da496e4b9ece00e3517a
Marcos Danilo <marcosdanilo94@gmail.com>
2022-12-12 16:42:18 -0300

    feat: implementa a função exists_word no módulo word_search
    a função recebe a palavra a ser buscada e a fila em que deverá fazer a busca e retorna uma lista com dicionários que contém a palavra buscada, o arquivo que possui as ocorrências e as linhas onde aparece a palavra buscada

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,34 @@
+# from ting_file_management.file_process import process
+# from ting_file_management.queue import Queue
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    occurrencies_index = []
+
+    for data in instance:
+        for index in range(len(data["linhas_do_arquivo"])):
+            if word.lower() in data["linhas_do_arquivo"][index].lower():
+                occurrencies_index.append(index + 1)
+
+        result.append(
+            {
+                "palavra": word.lower(),
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": [
+                    {"linha": index} for index in occurrencies_index
+                ],
+            }
+        )
+
+    return result if len(occurrencies_index) > 0 else []
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+
+# fila = Queue()
+# process("statics/arquivo_teste.txt", fila)
+# if __name__ == "__main__":
+#     exists_word("de", fila)
