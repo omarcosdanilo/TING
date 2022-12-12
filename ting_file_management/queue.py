7eb7e82f1688b663586b61a065c6f1ec4854f9ac
Marcos Danilo <marcosdanilo94@gmail.com>
2022-12-12 16:39:21 -0300

    feat: cria um iterator para a classe Queue
    Cria um iterator na classe Queue para permitir iterar sobre os elementos da fila

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -4,12 +4,25 @@ from collections import deque
 class Queue:
     def __init__(self):
         self.__data = deque()
+        self.__position = 0
 
     def __repr__(self) -> str:
         return str(self.__data)
 
     def __len__(self):
         return len(self.__data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            value = self.__data[self.__position]
+            self.__position += 1
+        except IndexError:
+            raise StopIteration()
+
+        return value
 
     def enqueue(self, value):
         return self.__data.append(value)
